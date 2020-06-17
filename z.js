7b05d67860185aef10c797a6af50e3e51c60e368
vale151 <malbranv@gmail.com>
2020-06-17 20:35:35 -0300

    Intetando hacer  el punto 7

--- z.js
+++ z.js
@@ -1,2 +1,6 @@
 console.log("blabla");
-var saludo= "Hola, estoy haciendo el punto 6 :p"
+var saludo = "Hola, estoy haciendo el punto 6 :p"
+console.log(saludo);
+
+let saludo2 = "Hola Santi, estoy probando lo del punto 7 :D";
+console.log(saludo2);
